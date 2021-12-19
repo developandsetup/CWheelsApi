75335cbe06317f9e6853d0266c69b4592a680ad7
Danko Papucic <danko.bananko2021@outlook.com>
2021-12-19 17:44:49 +0000

    create Db

---
+++ CWheelsApi/Data/CWheelsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CWheelsApi.Data
+{
+    public class CWheelsDbContext
+    {
+    }
+}
